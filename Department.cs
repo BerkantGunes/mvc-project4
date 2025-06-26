2076267d8072cc49c30b5b78603b2371732c6d4d
Berkant Gunes <gunesberkant96@gmail.com>
2025-06-26 21:28:25 +0300

    Add files via upload

---
+++ Department.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Fabrika.Data.Model
+{
+    [Table("Departments")]
+    public class Department
+    {
+        [Key]
+        public int DepartmentId { get; set; }
+        public string Name { get; set; }
+        public int FactoryId { get; set; }
+        public Factory Factory { get; set; }
+    }
+}
---
+++ Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fabrika.Data.Model
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string FullName { get; set; }
+        public string Role { get; set; }
+        public int DepartmentId { get; set; }
+        public Department Department { get; set; }
+    }
+}
