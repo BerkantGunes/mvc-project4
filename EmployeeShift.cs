792730abb547faa2862c3cea323a13b2fa7b7a9c
Berkant Gunes <gunesberkant96@gmail.com>
2025-06-26 21:28:39 +0300

    Add files via upload

---
+++ EmployeeShift.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fabrika.Data.Model
+{
+    public class EmployeeShift
+    {
+        public int Id { get; set; }
+        public int EmployeeId { get; set; }
+        public int ShiftId { get; set; }
+        public DateTime ShiftDate { get; set; }
+        public Employee Employee { get; set; }
+        public Shift Shift { get; set; }
+    }
+}
---
+++ Factory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fabrika.Data.Model
+{
+    public class Factory
+    {
+        public int FactoryId { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public ICollection<Department> Departments { get; set; }
+    }
+}
