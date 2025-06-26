ddd9a81f55e048de16106df52ff257dc801a7cc1
Berkant Gunes <gunesberkant96@gmail.com>
2025-06-26 21:28:09 +0300

    Add files via upload

---
+++ dataContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fabrika.Data.Context
+{
+    public class dataContext:DbContext
+    {
+        public DbSet< MyProperty { get; set; }
+    }
+}
