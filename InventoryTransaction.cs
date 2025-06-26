00c05cf2e41973a3b7c144663c7e62dffac1a746
Berkant Gunes <gunesberkant96@gmail.com>
2025-06-26 21:29:15 +0300

    Add files via upload

---
+++ InventoryTransaction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fabrika.Data.Model
+{
+    public class InventoryTransaction
+    {
+        public int InventoryTransactionId { get; set; }
+        public int InventoryItemId { get; set; }
+        public DateTime Date { get; set; }
+        public double Quantity { get; set; }
+        public string TransactionType { get; set; } // IN, OUT, ADJUSTMENT
+        public InventoryItem InventoryItem { get; set; }
+    }
+}
---
+++ Machine.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fabrika.Data.Model
+{
+    public class Machine
+    {
+        public int MachineId { get; set; }
+        public string Name { get; set; }
+        public string SerialNumber { get; set; }
+        public DateTime PurchaseDate { get; set; }
+        public int DepartmentId { get; set; }
+        public Department Department { get; set; }
+    }
+}
