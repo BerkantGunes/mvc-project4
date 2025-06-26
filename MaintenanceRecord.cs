6a1830d8a19de4a34324300ea1e79bb7796acb67
Berkant Gunes <gunesberkant96@gmail.com>
2025-06-26 21:29:34 +0300

    Add files via upload

---
+++ MaintenanceRecord.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fabrika.Data.Model
+{
+    public class MaintenanceRecord
+    {
+        public int MaintenanceRecordId { get; set; }
+        public int MachineId { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public string PerformedBy { get; set; }
+        public Machine Machine { get; set; }
+    }
+}
---
+++ Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fabrika.Data.Model
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductCode { get; set; }
+        public string Name { get; set; }
+        public string Unit { get; set; } // e.g., kg, piece
+    }
+}
