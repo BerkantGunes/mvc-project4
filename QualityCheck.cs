256b2e6d2b3d22d491d26773c3ff5b28c294d41b
Berkant Gunes <gunesberkant96@gmail.com>
2025-06-26 21:30:15 +0300

    Add files via upload

---
+++ QualityCheck.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fabrika.Data.Model
+{
+    public class QualityCheck
+    {
+        public int QualityCheckId { get; set; }
+        public int ProductId { get; set; }
+        public DateTime CheckDate { get; set; }
+        public string Result { get; set; } // Pass, Fail
+        public string Notes { get; set; }
+        public Product Product { get; set; }
+    }
+}
---
+++ Shift.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fabrika.Data.Model
+{
+    public class Shift
+    {
+        public int ShiftId { get; set; }
+        public string Name { get; set; } // e.g., "Morning", "Night"
+        public TimeSpan Start { get; set; }
+        public TimeSpan End { get; set; }
+    }
+}
