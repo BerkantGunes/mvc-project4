cd05d541926375c21752195f7b17020746c18b43
Berkant Gunes <gunesberkant96@gmail.com>
2025-06-26 21:28:56 +0300

    Add files via upload

---
+++ IncidentReport.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fabrika.Data.Model
+{
+    public class IncidentReport
+    {
+        public int IncidentReportId { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public string ReportedBy { get; set; }
+    }
+}
---
+++ InventoryItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fabrika.Data.Model
+{
+    public class InventoryItem
+    {
+        public int InventoryItemId { get; set; }
+        public string ItemCode { get; set; }
+        public string Description { get; set; }
+        public string Unit { get; set; }
+        public double Quantity { get; set; }
+    }
+}
