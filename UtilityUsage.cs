9000b2052e23147a5ecc1e1fa45eb2279524fe04
Berkant Gunes <gunesberkant96@gmail.com>
2025-06-26 21:30:31 +0300

    Add files via upload

---
+++ UtilityUsage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fabrika.Data.Model
+{
+    public class UtilityUsage
+    {
+        public int UtilityUsageId { get; set; }
+        public DateTime Date { get; set; }
+        public string Type { get; set; } // Electricity, Water, Gas
+        public double Amount { get; set; }
+    }
+}
---
+++ VisitorLog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fabrika.Data.Model
+{
+    public class VisitorLog
+    {
+        public int VisitorLogId { get; set; }
+        public string VisitorName { get; set; }
+        public DateTime VisitDate { get; set; }
+        public string Purpose { get; set; }
+    }
+}
